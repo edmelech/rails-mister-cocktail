0e0cb67f29d6195eaab1f382d968dbc64c8caeb4
Ed Melech <mr.melech@gmail.com>
2021-02-18 16:44:57 +0000

    working on features. step 3/4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+ingredients = JSON.parse(response)
+
+ingredient_list = ingredients['drinks']
+
+ingredient_list.each do |list|
+  Ingredient.create(name: list['strIngredient1'])
+end
+
+Cocktail.create(name: "Whiskey Sour")
+Cocktail.create(name: "Espresso Martini")
+Cocktail.create(name: "Old Fashioned")
