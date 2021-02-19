3194883d36194407513cde357cc1a2d2d610be9a
Ed Melech <mr.melech@gmail.com>
2021-02-19 15:26:16 +0000

    added all controller actions and views. Not checked if they are working yet

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,4 +8,21 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
   end
 
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+      if @garden.save
+        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
+        format.json { render :show, status: :created, location: @cocktail }
+      else
+        format.html { render :new }
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :ingredients)
+  end
 end
